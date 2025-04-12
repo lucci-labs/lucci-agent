3a3e0f4170f7a82fa7e7488b16e008fe244f5781
imduchuyyy <duchuy.124dk@gmail.com>
2025-04-12 15:24:55 +0700

    feat: add TwitterClient initialization with cookie handling and environment variable checks

--- clients/twitter.ts
+++ clients/twitter.ts
@@ -1,4 +1,5 @@
 import { Scraper } from "agent-twitter-client";
+const fs = require("fs");
 
 const twitterCookies = "twitter-cookies";
 
@@ -8,5 +9,23 @@ export class TwitterClient {
   constructor() {
     this.scraper = new Scraper();
     this.scraper.getCookies
+  }
+
+  init = async () => {
+    if (fs.existsSync(twitterCookies)) {
+      const cookies = fs.readFileSync(twitterCookies, "utf-8");
+      await this.scraper.setCookies(cookies);
+    } else {
+      if (!process.env.TWITTER_USERNAME) {
+        throw new Error("env missing: TWITTER_USERNAME is not set");
+      }
+      if (!process.env.TWITTER_PASSWORD) {
+        throw new Error("env missing: TWITTER_PASSWORD is not set");
+      }
+
+      await this.scraper.login(process.env.TWITTER_USERNAME, process.env.TWITTER_PASSWORD);
+      const cookies = await this.scraper.getCookies();
+      fs.writeFileSync(twitterCookies, cookies, "utf-8");
+    }
   }
 }
